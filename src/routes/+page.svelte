2849034ea484ca8404042d2ac246da1b89ac9caf
slylealdrich <slylealdrich@gmail.com>
2024-06-21 01:08:23 -0500

    updated look a little

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -19,39 +19,49 @@
 
 <h1 class="text-xl font-bold">Budget App</h1>
 
-<div class="p-16">
-	<SuperDebug data={$addEntryForm} theme="vscode" />
+<div class="divide-y">
+	<form
+		use:addEntryEnhance
+		method="post"
+		action="?/addEntry"
+		autocomplete="off"
+		class="w-full h-8 flex justify-start items-center gap-x-4"
+	>
+		<label for="description">Description:</label>
+		<input
+			name="description"
+			type="text"
+			placeholder="describe the purchase"
+			bind:value={$addEntryForm.description}
+			class="px-1 bg-gray-200"
+		/>
+
+		<label for="dollars">Amount:</label>
+		<input
+			name="dollars"
+			type="string"
+			bind:value={$addEntryForm.dollars}
+			class="px-1 bg-gray-200"
+		/>
+
+		<label for="date">Date of Purchase:</label>
+		<input name="date" type="string" bind:value={$addEntryForm.date} class="px-1 bg-gray-200" />
+
+		<button type="submit" class="w-12 bg-blue-200"> Add </button>
+	</form>
+
+	{#each data.entries as entry}
+		<div
+			class="w-full h-8 flex justify-start items-center gap-x-4 odd:bg-gray-100 even:bg-gray-200"
+		>
+			<p>{format(entry.date, 'MM-dd-yyyy')}</p>
+			<p>{entry.description}</p>
+			<p>{entry.cents / 100n}.{entry.cents % 100n}</p>
+
+			<form use:deleteEntryEnhance method="post" action="?/deleteEntry">
+				<input name="id" type="string" value={entry.id} class="hidden" />
+				<button class="w-16 bg-red-200">Delete</button>
+			</form>
+		</div>
+	{/each}
 </div>
-<form use:addEntryEnhance method="post" action="?/addEntry" class="border">
-	<label for="description">Description</label>
-	<input
-		name="description"
-		type="text"
-		placeholder="describe the purchase"
-		bind:value={$addEntryForm.description}
-	/>
-
-	<label for="dollars">Dollars</label>
-	<input name="dollars" type="string" bind:value={$addEntryForm.dollars} />
-
-	<label for="date">Date of Purchase</label>
-	<input name="date" type="string" bind:value={$addEntryForm.date} />
-
-	<button type="submit"> Add </button>
-	{#if $addEntryErrors}
-		<p>{$addEntryMessage}</p>
-	{/if}
-</form>
-
-{#each data.entries as entry}
-	<div class="border">
-		<p>{format(entry.date, 'MM-dd-yyyy')}</p>
-		<p>{entry.description}</p>
-		<p>{entry.cents / 100n}.{entry.cents % 100n}</p>
-
-		<form use:deleteEntryEnhance method="post" action="?/deleteEntry">
-			<input name="id" type="string" value={entry.id} class="hidden" />
-			<button>Delete</button>
-		</form>
-	</div>
-{/each}
