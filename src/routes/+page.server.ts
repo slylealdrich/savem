27b53ee551c99c027b23f321c1c5650f23a14070
slylealdrich <slylealdrich@gmail.com>
2024-06-29 16:56:42 -0500

    moved add entry form to a component and swapped to postgresql

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -5,21 +5,7 @@ import { message, superValidate } from 'sveltekit-superforms';
 import { zod } from 'sveltekit-superforms/adapters';
 import { z } from 'zod';
 import type { PageServerLoad } from './$types.js';
-import { isValid, parse } from 'date-fns';
-
-const addEntryFormSchema = z
-	.object({
-		description: z.string(),
-		dollars: z.number(),
-		cents: z.number().min(1).max(99),
-		month: z.number(),
-		day: z.number(),
-		year: z.number()
-	})
-	.refine((schema) => {
-		const date = parse(schema.year + '-' + schema.month + '-' + schema.day, 'yyyy-M-d', new Date());
-		return isValid(date);
-	}, 'Invalid date');
+import { addEntrySchema } from '$lib/schemas.js';
 
 const deleteEntryFormSchema = z.object({
 	id: z.string()
@@ -36,7 +22,7 @@ export const load: PageServerLoad = async () => {
 		year: today.getFullYear()
 	};
 
-	const addEntryForm = await superValidate(addEntryFormSeed, zod(addEntryFormSchema));
+	const addEntryForm = await superValidate(addEntryFormSeed, zod(addEntrySchema));
 	const deleteEntryForm = await superValidate(zod(deleteEntryFormSchema));
 
 	const entries: Entry[] = await prisma.entry.findMany({
@@ -54,13 +40,14 @@ export const load: PageServerLoad = async () => {
 
 export const actions = {
 	addEntry: async ({ request }) => {
-		const form = await superValidate(request, zod(addEntryFormSchema));
-		console.log(form);
+		const form = await superValidate(request, zod(addEntrySchema));
+
 		if (!form.valid) return fail(400, { form });
+
 		await prisma.entry.create({
 			data: {
 				description: form.data.description,
-				cents: (form.data.dollars * 100) + form.data.cents,
+				cents: form.data.dollars * 100 + form.data.cents,
 				date: new Date(form.data.year, form.data.month - 1, form.data.day)
 			}
 		});
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,81 +1,15 @@
 <script lang="ts">
+	import AddEntryForm from '$lib/components/AddEntryForm.svelte';
 	import { format } from 'date-fns';
 	import { superForm } from 'sveltekit-superforms';
-	import SuperDebug from 'sveltekit-superforms';
 
 	const { data } = $props();
 
-	const {
-		form: addEntryForm,
-		errors: addEntryErrors,
-		enhance: addEntryEnhance,
-		message: addEntryMessage
-	} = superForm(data.addEntryForm, {
-		resetForm: true
-	});
-
-	const { form: deleteEntryForm, enhance: deleteEntryEnhance } = superForm(data.deleteEntryForm);
+	const { enhance: deleteEntryEnhance } = superForm(data.deleteEntryForm);
 </script>
 
 <div class="divide-y">
-	<form
-		use:addEntryEnhance
-		method="post"
-		action="?/addEntry"
-		autocomplete="off"
-		class="w-full h-12 px-4 flex justify-start items-center gap-x-4 bg-emerald-800"
-	>
-		<div>
-			<label for="description">Description:</label>
-			<input
-				name="description"
-				type="text"
-				bind:value={$addEntryForm.description}
-				class="px-1 bg-emerald-100"
-			/>
-		</div>
-
-		<label for="amount">Amount:</label>
-		<div>
-			$
-			<input
-				name="dollars"
-				bind:value={$addEntryForm.dollars}
-				class="w-16 px-1 bg-emerald-100 text-right"
-			/>
-			.
-			<input
-				name="cents"
-				bind:value={$addEntryForm.cents}
-				class="w-8 px-1 bg-emerald-100 text-left"
-			/>
-		</div>
-
-		<label for="date">Date of Purchase:</label>
-		<div>
-			<input
-				name="month"
-				bind:value={$addEntryForm.month}
-				class="w-7 px-1 bg-emerald-100 text-right"
-			/>
-			/
-			<input
-				name="day"
-				bind:value={$addEntryForm.day}
-				class="w-7 px-1 bg-emerald-100 text-center"
-			/>
-			/
-			<input
-				name="year"
-				bind:value={$addEntryForm.year}
-				class="w-12 px-1 bg-emerald-100 text-left"
-			/>
-		</div>
-
-		<button type="submit" class="w-8 h-8 bg-emerald-600 text-emerald-100">
-			<i class="fa-solid fa-plus"></i>
-		</button>
-	</form>
+	<AddEntryForm data={data.addEntryForm} />
 
 	{#each data.entries as entry}
 		<div
