2b0abd0e9d0d66e119e77da2a90328f37dd520cb
slylealdrich <slylealdrich@gmail.com>
2024-07-06 08:33:58 -0500

    added Modal.svelte from other project

---
+++ src/lib/components/Modal.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import { createEventDispatcher } from "svelte";
+  import { fade } from "svelte/transition";
+
+  let backdrop: HTMLDivElement;
+
+  const dispatch = createEventDispatcher<{ offClick: void }>();
+
+  function handleBackdropClick(event: MouseEvent) {
+    if (event.target === backdrop) dispatch("offClick");
+  }
+</script>
+
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<!-- svelte-ignore a11y-no-static-element-interactions -->
+<div
+  bind:this={backdrop}
+  on:click={handleBackdropClick}
+  transition:fade|local={{ duration: 50 }}
+  class="fixed inset-0 z-50 flex justify-center items-center bg-black/50 backdrop-blur"
+>
+  <div class="overflow-x-hidden overflow-y-auto">
+    <slot />
+  </div>
+</div>
