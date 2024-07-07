1bcee22dafdc0f86157358b09e08d0ddc429f7b7
slylealdrich <slylealdrich@gmail.com>
2024-07-07 15:53:07 -0500

    refactored Modal for svelte 5 and added confirmation on delete

--- src/lib/components/Entry.svelte
+++ src/lib/components/Entry.svelte
@@ -3,12 +3,15 @@
   import { type Entry } from "@prisma/client";
   import { format } from "date-fns";
   import { superForm, type Infer, type SuperValidated } from "sveltekit-superforms";
+  import Modal from "./Modal.svelte";
 
   const {
     data,
     deleteForm,
   }: { data: Entry; deleteForm: SuperValidated<Infer<DeleteEntryFormSchema>> } = $props();
-  const { enhance: deleteEntryEnhance } = superForm(deleteForm);
+  const { enhance: deleteEntryEnhance, submit: submitDeleteEntryForm } = superForm(deleteForm);
+
+  let deleteConfirmationModalVisible = $state(false);
 </script>
 
 <div class="p-2 flex items-center bg-emerald-800 text-center text-emerald-200 rounded-md">
@@ -30,8 +33,30 @@
     class="basis-1/12 w-full flex justify-center items-center"
   >
     <input name="id" type="string" value={data.id} class="hidden" />
-    <button type="submit">
-      <i class="fa-solid fa-xmark text-emerald-700"></i>
+    <button type="button" onclick={() => (deleteConfirmationModalVisible = true)}>
+      <i class="fa-solid fa-xmark text-emerald-700 text-xl"></i>
     </button>
   </form>
 </div>
+
+<!-- Confirmation -->
+{#if deleteConfirmationModalVisible}
+  <Modal offClick={() => (deleteConfirmationModalVisible = false)}>
+    <div class="w-[80dvw] p-10 flex flex-col gap-y-4 bg-emerald-800 text-emerald-200 rounded-md">
+      <span class="text-center">Are you sure you want to delete this?</span>
+      <div class="h-8 flex gap-x-2">
+        <button
+          onclick={() => (deleteConfirmationModalVisible = false)}
+          class="basis-1/2 h-full bg-emerald-900 rounded-md">No</button
+        >
+        <button
+          onclick={() => {
+            submitDeleteEntryForm();
+            deleteConfirmationModalVisible = false;
+          }}
+          class="basis-1/2 h-full bg-emerald-700 rounded-md">Yes</button
+        >
+      </div>
+    </div>
+  </Modal>
+{/if}
--- src/lib/components/Modal.svelte
+++ src/lib/components/Modal.svelte
@@ -1,25 +1,25 @@
 <script lang="ts">
-  import { createEventDispatcher } from "svelte";
+  import { type Snippet } from "svelte";
   import { fade } from "svelte/transition";
 
   let backdrop: HTMLDivElement;
 
-  const dispatch = createEventDispatcher<{ offClick: void }>();
+  const { children, offClick }: { children: Snippet; offClick: () => void } = $props();
 
   function handleBackdropClick(event: MouseEvent) {
-    if (event.target === backdrop) dispatch("offClick");
+    if (event.target === backdrop) offClick();
   }
 </script>
 
-<!-- svelte-ignore a11y-click-events-have-key-events -->
-<!-- svelte-ignore a11y-no-static-element-interactions -->
+<!-- svelte-ignore a11y_click_events_have_key_events -->
+<!-- svelte-ignore a11y_no_static_element_interactions -->
 <div
   bind:this={backdrop}
-  on:click={handleBackdropClick}
+  onclick={handleBackdropClick}
   transition:fade|local={{ duration: 50 }}
   class="fixed inset-0 z-50 flex justify-center items-center bg-black/50 backdrop-blur"
 >
   <div class="overflow-x-hidden overflow-y-auto">
-    <slot />
+    {@render children()}
   </div>
 </div>
